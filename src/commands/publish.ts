2bb887ff243dd6c9eccb6dc32fe2d78f0894527d
jiyuu-jin <zach@valist.io>
2024-02-27 14:54:52 -0700

    Add CLI commands

---
+++ src/commands/publish.ts
@@ -0,0 +1,111 @@
+import { Command, CliUx } from '@oclif/core';
+import { ethers } from 'ethers';
+import { create, ReleaseConfig } from '@valist/sdk';
+import YAML from 'yaml';
+import * as fs from 'node:fs';
+import * as flags from '../flags';
+import { select } from '../keys';
+
+export default class Publish extends Command {
+  static provider?: ethers.Signer;
+
+  static description = 'Publish a release'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %> ipfs/go-ipfs/v0.12.3 README.md',
+    '<%= config.bin %> <%= command.id %> ipfs/go-ipfs/v0.12.3 dist/',
+  ]
+
+  static flags = {
+    'meta-tx': flags.metaTx,
+    'network': flags.network,
+    'private-key': flags.privateKey,
+  }
+
+  static args = [
+    {
+      name: 'package',
+      description: 'package name',
+    },
+    {
+      name: 'path',
+      description: 'path to artifact file or directory',
+    },
+  ]
+
+  async provider(network: string, privateKey: string): Promise<ethers.Signer> {
+    if (Publish.provider) return Publish.provider;
+
+    const provider = new ethers.providers.JsonRpcProvider(network);
+    return new ethers.Wallet(privateKey, provider);
+  }
+
+  public async run(): Promise<void> {
+    const { args, flags } = await this.parse(Publish);
+
+    let config: ReleaseConfig;
+    if (args.package) {
+      const parts = args.package.split('/');
+      if (parts.length !== 3) this.error('invalid package name');
+      config = new ReleaseConfig(parts[0], parts[1], parts[2]);
+      config.platforms['web'] = args.path;
+    } else {
+      const data = fs.readFileSync('hyperplay.yml', 'utf8');
+      config = YAML.parse(data);
+    }
+
+    if (!config.account) this.error('invalid account name');
+    if (!config.project) this.error('invalid project name');
+    if (!config.release) this.error('invalid release name');
+    if (!config.platforms) this.error('no platforms configured');
+
+    const privateKey = flags['private-key'] || await select();
+    const metaTx = flags['meta-tx'];
+
+    const provider = await this.provider(flags.network, privateKey);
+    const valist = await create(provider, { metaTx });
+
+    const address = await provider.getAddress();
+    const chainId = await provider.getChainId();
+
+    const accountID = valist.generateID(chainId, config.account);
+    const projectID = valist.generateID(accountID, config.project);
+    const releaseID = valist.generateID(projectID, config.release);
+
+    const isAccountMember = await valist.isAccountMember(accountID, address);
+    const isProjectMember = await valist.isProjectMember(projectID, address);
+
+    if (!(isAccountMember || isProjectMember)) {
+      this.error('user is not an account or project member');
+    }
+
+    const releaseExists = await valist.releaseExists(releaseID);
+    if (releaseExists) {
+      this.error(`release ${config.release} exists`);
+    }
+
+    CliUx.ux.action.start('uploading files');
+    const release = await valist.uploadRelease(config);
+    CliUx.ux.action.stop();
+
+    CliUx.ux.log(`successfully uploaded files to IPFS: ${release.external_url}`);
+
+    CliUx.ux.action.start('publishing release');
+    const tx = await valist.createRelease(projectID, config.release, release);
+    CliUx.ux.action.stop();
+
+    CliUx.ux.action.start(`confirming transaction ${tx.hash}`);
+    await tx.wait();
+    CliUx.ux.action.stop();
+
+    CliUx.ux.log(`successfully published ${config.account}/${config.project}/${config.release}!`);
+
+    CliUx.ux.log(`view the release at:
+    https://developers.hyperplay.xyz/${config.account}/${config.project}/settings
+    ${release.external_url}
+    ipfs://${release.external_url.replace('https://gateway.valist.io/ipfs/', '')}
+    `);
+
+    this.exit(0);
+  }
+}
